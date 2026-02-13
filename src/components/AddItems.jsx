fa627824e6c2d76afd0044010b31d202b6964737
Jardollyno <jardelvinicius38@gmail.com>
2026-02-13 00:24:03 -0300

    Atualiza Componente AddItems

--- src/components/AddItems.jsx
+++ src/components/AddItems.jsx
@@ -12,37 +12,64 @@ function AddItems({ onAddItemSubmit, onClose }) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
-      <div className="bg-slate-400 w-full max-w-md rounded-xl shadow-2xl p-6 relative">
+      <div className="bg-slate-400 w-full max-w-md rounded-xl shadow-2xl p-6 m-2 relative">
         <Title>Adicionar Item</Title>
-        <Input
-          type="text"
-          placeholder="Nome do produto..."
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        ></Input>
-        <Input
-          type="text"
-          placeholder="Categoria do produto..."
-          value={category}
-          onChange={(event) => setCategory(event.target.value)}
-        ></Input>
-        <Input
-          type="number"
-          placeholder="Quantidade do produto..."
-          value={quantity}
-          onChange={(event) => setQuantity(event.target.value)}
-        ></Input>
-        <Input
-          type="number"
-          placeholder="Quantidade mínima do produto..."
-          value={minimum}
-          onChange={(event) => setMinimum(event.target.value)}
-        ></Input>
+        <div className="flex flex-row items-center gap-2">
+          <p className="w-64 text-xl text-slate-100 font-semibold text-left">
+            Nome:
+          </p>
+          <Input
+            type="text"
+            placeholder="Nome do produto..."
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          ></Input>
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <p className="w-64 text-xl text-slate-100 font-semibold text-left">
+            Quantidade:
+          </p>
+          <Input
+            type="number"
+            placeholder="Quantidade do produto..."
+            value={quantity}
+            onChange={(event) => setQuantity(event.target.value)}
+          ></Input>
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <p className="w-64 text-xl text-slate-100 font-semibold text-left">
+            Mínimo:
+          </p>
+          <Input
+            type="number"
+            placeholder="Quantidade mínima..."
+            value={minimum}
+            onChange={(event) => setMinimum(event.target.value)}
+          ></Input>
+        </div>
+        <div className="flex flex-row items-center gap-2">
+          <p className="w-64 text-xl text-slate-100 font-semibold text-left">
+            Categoria:
+          </p>
+          <select
+            id="category"
+            name="category"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+            className="w-full text-right mt-2 border border-slate-300 text-slate-400
+            px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+          >
+            <option value="">Selecione uma categoria</option>
+            <option value="Agulhas">Agulhas</option>
+            <option value="Biqueiras">Biqueiras</option>
+            <option value="Descartáveis">Descartáveis</option>
+          </select>
+        </div>
         <div className="flex justify-center gap-4 mt-2">
           <Button
             onClick={() => {
               // verificar campos vazios
-              if (!name.trim() || !category.trim() || !quantity || !minimum) {
+              if (!name.trim() || !quantity || !minimum) {
                 alert("Por favor, preencha todos os campos.");
                 return;
               }
